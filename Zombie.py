e4973fdbf1c2a81fedba6453ac7851817803cf62
Sid Domala <sdomala@thehill.org>
2018-11-21 18:50:52 -0500

    Create Zombie.py

---
+++ Zombie.py
@@ -0,0 +1,18 @@
+# Zombie class for Term Project
+# Name: Saisiddarth Domala
+# andrewID: sdomala
+# Section: O
+# Last modified 11/21/18
+
+
+from Enemy import *
+
+# Zombie class that inherits and overrides Enemy properties/methods
+
+class Zombie (Enemy) :
+    def __init__(self, x, y, rows, cols, margin, width, height, stepY):
+        super().__init__ (x, y, rows, cols, margin, width, height, stepY)
+        print (3)
+        self.image = pygame.image.load ('GreenZombie.png').convert_alpha()        
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
