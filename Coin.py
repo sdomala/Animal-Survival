6c32bf8d9754d1f97c4dfdf86c267a0af598eb21
Sid Domala <sdomala@thehill.org>
2018-11-26 00:37:37 -0500

    Create Coin.py

---
+++ Coin.py
@@ -0,0 +1,33 @@
+# Coin class for Term Project
+# Name: Saisiddarth Domala
+# andrewID: sdomala
+# Section: O
+# Last modified 11/25/18
+
+import pygame
+import random
+import math
+
+
+# Animal class with properties and methods for domesticated and wild animals
+
+class Coin(pygame.sprite.Sprite):
+
+# Constructor method that initializes monster image, location, and speed
+    def __init__(self, x, y):
+        super(Coin, self).__init__()
+        self.x, self.y = x, y
+        self.image = pygame.image.load ('Coin.png').convert_alpha()        
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.rect = pygame.Rect(self.x - self.width//2, self.y - self.height//2,
+                                self.width, self.height)
+       
+    
+
+# Method that re-stores the x and y coordinates and the width and height of 
+# the image
+
+    def getRect(self): 
+        self.rect = pygame.Rect(self.x - self.width//2, self.y - self.height//2,
+                                self.width, self.height)
