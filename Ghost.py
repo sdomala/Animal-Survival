0d4d1825e6e940dc4fd7dd7f2856356e02c621fa
Sid Domala <sdomala@thehill.org>
2018-11-27 19:26:47 -0500

    Update Ghost.py

--- Ghost.py
+++ Ghost.py
@@ -13,8 +13,8 @@ from Enemy import *
 # Ghost class that inherits and overrides Enemy properties/methods
 
 class Ghost (Enemy) :
-    def __init__(self, x, y, rows, cols, margin, width, height, stepY, plantBlocks):
-        super().__init__ (x, y, rows, cols, margin, width, height, stepY, plantBlocks)
+    def __init__(self, x, y, rows, cols, margin, width, height, stepY, plantBlocks, grassSlot, direction):
+        super().__init__ (x, y, rows, cols, margin, width, height, stepY, stepX, plantBlocks, grassSlot, direction)
         self.image = pygame.image.load ('Ghost.png').convert_alpha()        
         self.width = self.image.get_width()
         self.height = self.image.get_height()
