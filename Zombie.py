39fc57e1d4070492c114ddc32e9182a398414a20
Sid Domala <sdomala@thehill.org>
2018-12-05 15:56:11 -0500

    Update Zombie.py

--- Zombie.py
+++ Zombie.py
@@ -13,8 +13,8 @@ from Enemy import *
 # Zombie class that inherits and overrides Enemy properties/methods
 
 class Zombie (Enemy) :
-    def __init__(self, x, y, rows, cols, margin, width, height, stepY, stepX, plantBlocks, grassSlot, direction):
-        super().__init__ (x, y, rows, cols, margin, width, height, stepY, stepX, plantBlocks, grassSlot, direction)
+    def __init__(self, x, y, rows, cols, margin, width, height, stepY, stepX, plantBlocks, grassSlot, direction, speed):
+        super().__init__ (x, y, rows, cols, margin, width, height, stepY, stepX, plantBlocks, grassSlot, direction, speed)
         self.image = pygame.image.load ('GreenZombie4.png').convert_alpha()        
         self.width = self.image.get_width()
         self.height = self.image.get_height()
