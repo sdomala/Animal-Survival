f9b146080f796eb27bbe3df2cd02e98b15282472
Sid Domala <sdomala@thehill.org>
2018-12-06 01:41:29 -0500

    Update Monster.py

--- Monster.py
+++ Monster.py
@@ -2,7 +2,7 @@
 # Name: Saisiddarth Domala
 # andrewID: sdomala
 # Section: O
-# Last modified 11/21/18
+# Last modified 12/5/18
 
 # Citation: Got image from
 # https://friendlystock.com/product/mud-monster/
