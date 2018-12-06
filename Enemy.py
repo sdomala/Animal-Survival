6153b262c2fe21b8bee6618e626c1f95fc29f9bd
Sid Domala <sdomala@thehill.org>
2018-12-06 01:42:43 -0500

    Update Enemy.py

--- Enemy.py
+++ Enemy.py
@@ -2,7 +2,7 @@
 # Name: Saisiddarth Domala
 # andrewID: sdomala
 # Section: O
-# Last modified 11/23/18
+# Last modified 12/5/18
 
 # Citation: Got image from
 # https://techflourish.com/categories/halloween-zombie-clipart.html
