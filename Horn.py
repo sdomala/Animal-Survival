42e9381cc92ecadbccb7cf66dcca32217939715b
Sid Domala <sdomala@thehill.org>
2018-12-06 01:42:18 -0500

    Update Horn.py

--- Horn.py
+++ Horn.py
@@ -2,7 +2,7 @@
 # Name: Saisiddarth Domala
 # andrewID: sdomala
 # Section: O
-# Last modified 11/26/18
+# Last modified 12/5/18
 
 # Citation: Got image from 
 # https://www.kisspng.com/png-horn-ram-trucks-goat-sheep-horn-711962/#
