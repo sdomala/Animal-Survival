beea70dad15bc2f6d687b225b2ab42ecb623e458
Sid Domala <sdomala@thehill.org>
2018-12-06 01:41:12 -0500

    Update Zombie.py

--- Zombie.py
+++ Zombie.py
@@ -2,7 +2,7 @@
 # Name: Saisiddarth Domala
 # andrewID: sdomala
 # Section: O
-# Last modified 11/21/18
+# Last modified 12/5/18
 
 # Citation, got image from 
 # https://techflourish.com/categories/halloween-zombie-clipart.html
